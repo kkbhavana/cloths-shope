8ae79543d916ebe161dd056098c2e70bfa81b7ba
kkbhavana <kkbhavana6@gmail.com>
2023-07-13 09:33:17 +0530

    ecom

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import ListCloths,DetailView
+from .views import ListCloths, DetailView, UpdateView, DeleteView
 
 urlpatterns = [
     path('cloth_list/',ListCloths.as_view(),name='list'),
     path('details/<int:pk>',DetailView.as_view(),name='details'),
+    path('update/<int:pk>', UpdateView.as_view(), name='details'),
+    path('delete/<int:pk>', DeleteView.as_view(), name='details'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 
 from .models import Cloths
@@ -11,22 +12,31 @@ from rest_framework import generics, status
 # product list
 
 class ListCloths(generics.ListCreateAPIView):
-    serializer_class = ClothSerializer
+    # permission_classes = [AllowAny]
     queryset = Cloths.objects.all()
+    serializer_class = ClothSerializer
 
-    def list(self, request, *args, **kwargs):
+    def list(self, request):
         queryset = self.get_queryset()
-        serializer = ClothSerializer(queryset,many=True)
+        serializer = ClothSerializer(queryset, many=True)
         return Response(serializer.data)
+
 
 class DetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ClothSerializer
     queryset = Cloths.objects.all()
 
-    def get(self,request,*args,**kwargs):
+    def get(self, request, *args, **kwargs):
         queryset = self.get_object()
         if queryset is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = ClothSerializer(queryset,many=False)
+        serializer = ClothSerializer(queryset, many=False)
         return Response(serializer.data)
 
+class UpdateView(generics.UpdateAPIView):
+    queryset = Cloths.objects.all()
+    serializer_class = ClothSerializer
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Cloths.objects.all()
+    serializer_class = ClothSerializer
